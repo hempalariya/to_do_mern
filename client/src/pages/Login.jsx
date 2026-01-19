a3f27358ba1fcc838dcf7d4e9f1cc5a2c79053c2
Hem Palariya <palariya8513@gmail.com>
2026-01-19 20:42:58 +0530

    break

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Button from '../components/Button'
 import WrapCard from '../components/WrapCard'
 
@@ -7,18 +7,29 @@ const labelClass = 'text-lg block'
 const inputClass = 'border border-blue-400 w-100 text-lg p-1 rounded-md'
 
 export default function Login() {
+
+    const [cred, setCred] = useState({
+        email:'',
+        password:''
+    })
+
+    function handleChange(e){
+        const {name, value} = e.target
+        setCred({...cred, [name]: value})
+    }
+
   return (
     <WrapCard>
             <form action="">
                 
                  <div className={controllerClass}>
                     <label htmlFor="email" className={labelClass}>Email</label>
-                    <input type="email" id = 'email' required className={inputClass}/>
+                    <input type="email" id = 'email' name='email' onChange={handleChange} required className={inputClass}/>
                 </div>
              
                 <div className={controllerClass}>
                     <label htmlFor="password" className={labelClass}>Password</label>
-                    <input type="password" id = 'password' required className={inputClass}/>
+                    <input type="password" id = 'password' name = 'password' onChange={handleChange} required className={inputClass}/>
                 </div>
                 <Button>Login</Button>
             </form>
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Button from '../components/Button'
 import WrapCard from '../components/WrapCard'
 
@@ -8,28 +8,37 @@ const inputClass = 'border border-blue-400 w-100 text-lg p-1 rounded-md'
 
 
 export default function Register() {
+    const [user, setUser] = useState({
+        fullname: '',
+        email: '',
+        number: '',
+        password: ''
+    }) 
+
+    const handleChange = (e) => {
+        const {name, value} = e.target
+        setUser({...user, [name]: value})
+    }
+
+
   return (
     <WrapCard>
         <form action="">
             <div className={controllerClass}>
                 <label htmlFor="fullname" className={labelClass}>Full Name</label>
-                <input type="text" id = 'fullname' required className={inputClass}/>
+                <input type="text" id = 'fullname' name='fullname' onChange={handleChange} required className={inputClass}/>
             </div>
              <div className={controllerClass}>
                 <label htmlFor="email" className={labelClass}>Email</label>
-                <input type="email" id = 'email' required className={inputClass}/>
+                <input type="email" id = 'email' name='email' onChange={handleChange} required className={inputClass}/>
             </div>
             <div className={controllerClass}>
                 <label htmlFor="number" className={labelClass}>Number</label>
-                <input type="text" id = 'number' required className={inputClass}/>
-            </div>
-            <div className={controllerClass}>
-                <label htmlFor="email" className={labelClass}>Email</label>
-                <input type="email" id = 'email' required className={inputClass}/>
+                <input type="text" id = 'number' name='number' onChange={handleChange} required className={inputClass}/>
             </div>
             <div className={controllerClass}>
                 <label htmlFor="password" className={labelClass}>Password</label>
-                <input type="password" id = 'password' required className={inputClass}/>
+                <input type="password" id = 'password' name='password' onChange={handleChange} required className={inputClass}/>
             </div>
             <Button>Register</Button>
         </form>
